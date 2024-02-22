7463375c05cff11fdacddd0d0b285ff75fa69af9
hitbheda <zayoxop666@gmail.com>
2024-02-22 11:28:19 +0530

    Hii!

---
+++ Practicle3/C/Keyw.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class rect{
+    String Name;
+    int Enrollment;
+    rect(String Name,int Enrollment){
+        this.Enrollment = Enrollment;
+        this.Name = Name;
+    }
+    void Display(){
+        System.out.println("Name Of Student = "+Name);
+        System.out.println("Enrollment Of Student = "+Enrollment);
+    }
+}
+
+public class Keyw{
+    public static void main(String[] args){
+        Scanner Obj = new Scanner(System.in);
+        System.out.print("Enter Name:");
+        String Name = Obj.nextLine();
+        System.out.print("Enter Name:");
+        int Enrollment = Obj.nextInt();
+        rect r1 = new rect(Name,Enrollment);
+        r1.Display();
+       react r2 = new react(Name , Enrollment);
+       r2.Display();
+    }
+}
