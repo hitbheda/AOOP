a6dc5e8b4a1195865a0ecdabe8447dbd266813f7
hitbheda <zayoxop666@gmail.com>
2024-05-06 12:15:06 +0530

    Hiii

---
+++ Practicle9/B/Main.java
@@ -0,0 +1,19 @@
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter File Name :");
+        String FileName = sc.nextLine();
+        try(FileOutputStream Obj = new FileOutputStream(FileName,false)){
+            System.out.print("Enter File Content :");
+            String Data = sc.nextLine();
+            byte[] arr = Data.getBytes();
+            Obj.write(arr);
+            Obj.close();
+        } catch (Exception e){
+            System.out.println("Error While Execution "+e);
+        }
+    }
+}
