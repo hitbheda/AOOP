a14e342fc2ca535c9c7820ed04800f9ac30b1115
hitbheda <zayoxop666@gmail.com>
2024-05-09 12:30:46 +0530

    hii

--- Practicle10/B/Main.java
+++ Practicle10/B/Main.java
@@ -2,9 +2,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put("Name","Hit");
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1,"Hit");
+        map.put(2,"Khelan");
+        map.put(3,"Nandan");
+        map.put(4,"Krishiv");
 
-        System.out.println(map);
+        for(int i:map.keySet()){
+            System.out.println("Key "+i+" Val "+map.get(i));
+        }
     }    
 }
