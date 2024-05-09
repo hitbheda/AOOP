65206378d7f8935c0b9bf970359b54ef1fc231d7
hitbheda <zayoxop666@gmail.com>
2024-05-09 12:07:19 +0530

    hii

--- Practicle10/B/Main.java
+++ Practicle10/B/Main.java
@@ -1,3 +1,10 @@
+import java.util.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("Name","Hit");
+
+        System.out.println(map);
+    }    
 }
