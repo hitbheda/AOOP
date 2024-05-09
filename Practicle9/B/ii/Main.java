643ac6dc403b49507fbae3124f140f9f19376145
hitbheda <zayoxop666@gmail.com>
2024-05-09 11:53:56 +0530

    hiit

--- Practicle9/B/ii/Main.java
+++ Practicle9/B/ii/Main.java
@@ -1,3 +1,12 @@
-public class Main {
-    
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        LinkedList<String> list1 = new LinkedList<String>();
+        String[] arr1 = {"January","February","March","April","May","June","July","August","Suptember"};
+        for(String month : arr1){
+            list1.add(month);
+        }
+        System.out.println(list1);
+    }
 }
